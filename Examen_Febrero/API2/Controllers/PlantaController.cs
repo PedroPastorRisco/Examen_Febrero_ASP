5a15b41ff9ac4d379ee5e37eeb6ed4137c090bfc
Pedro <krazyabyss@gmail.com>
2022-02-09 20:40:06 +0100

    Get hecho de plantas

---
+++ Examen_Febrero/API2/Controllers/PlantaController.cs
@@ -0,0 +1,63 @@
+﻿using Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.ObjectModel;
+using System.Net;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantaController : ControllerBase
+    {
+        // GET: api/<PlantaController>
+        [HttpGet]
+        public ObjectResult Get()
+        {
+            ObservableCollection<clsPlanta> plantas = null;
+            ObjectResult result = new ObjectResult(new { Value = plantas });
+
+            try
+            {
+                result.Value = DAL.Listados.clsListadoPlantaDAL.generarListadoPlantasDAL();
+                result.StatusCode = (int)HttpStatusCode.NotFound;
+                if ((result.Value as ObservableCollection<clsPlanta>) == null || (result.Value as ObservableCollection<clsPlanta>).Count == 0)
+                {
+                    result.StatusCode = (int)HttpStatusCode.NotFound;
+                }
+            }
+            catch (Exception)
+            {
+                result.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
+            return result;
+        }
+
+        // GET api/<PlantaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PlantaController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PlantaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PlantaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Examen_Febrero/API2/wwwroot/js/lostArk.js
@@ -0,0 +1,38 @@
+﻿window.onload = inicializaEventos;
+function inicializaEventos() {
+    rellenarTabla();
+}
+function rellenarTabla() {
+    var miLlamada = new XMLHttpRequest();
+    miLlamada.open("GET", "http://localhost:63921/api/planta");
+    miLlamada.onreadystatechange = function () {
+        if (miLlamada.readyState == 4 && miLlamada.status == 200) {
+            var arrayPlantas = JSON.parse(miLlamada.responseText);
+            for (var i = 0; i < arrayPlantas.length; i++) {
+                var fila = document.createElement("tr");
+                var columnaId = document.createElement("td");
+                var columnaName = document.createElement("td");
+                var columnaApellidos = document.createElement("td");
+                columnaId.innerHTML = arrayPlantas[i].idPlanta
+                columnaName.innerHTML = arrayPlantas[i].nombre
+                columnaApellidos.innerHTML = arrayPlantas[i].descripcion
+                fila.appendChild(columnaId)
+                fila.appendChild(columnaName)
+                fila.appendChild(columnaApellidos)
+                document.getElementById("tablaPlantas").appendChild(fila)
+            }
+        }
+        else {
+            controlarErrores(miLlamada.status);
+        }
+    }
+    miLlamada.send();
+}
+function controlarErrores(int) {
+    switch (int) {
+        case 404: document.getElementById("error").innerHTML = "SEXOOOOO"
+            break;
+        case 500: alert("Hey500")
+            break;
+    }
+}
